ee4c6b750d7ba07e9b472515a809e08d7d56dbad
youngchingjui <young.chingjui@gmail.com>
2023-03-30 13:27:03 +0800

    new mock data

--- data/mockData.js
+++ data/mockData.js
@@ -49,6 +49,36 @@ const posts = [
     likes: 20,
     views: 30,
   },
+  {
+    id: 5,
+    title: "Post 5",
+    prompt:
+      "chris hemsworth as thor, wearing Asgardian armor, feeding baby bottle to baby that he's cradling in his muscular arms, in living room littered with clothes and toys, baby throw up on his shoulder, baby is crying, Chris Hemsworth looking slightly stressed and disheveled --ar 16:9 --v 5 --q 2",
+    imageUrl:
+      "https://ai-prompt-newsfeed.s3.us-west-2.amazonaws.com/Ching_chris_hemsworth_as_thor_wearing_Asgardian_armor_feeding_b_24d3fb48-5e5d-47c0-b533-911f470b685f.png",
+    likes: 20,
+    views: 30,
+  },
+  {
+    id: 6,
+    title: "Post 6",
+    prompt:
+      "Editorial style front-view medium-full photo shot on Fujifilm Pro 400H of a 5-year-old chinese boy pondering a masterpiece in a Musée d'Orsay Gallery. He's wearing a blue jacquard blazer by Ferragamo with a white button-up. The soft gallery lighting and careful composition evoke a sense of refined luxury and creative curiosity as he gazes fixedly at the magnificent painting in front of him --ar 17:22 --v 5",
+    imageUrl:
+      "https://ai-prompt-newsfeed.s3.us-west-2.amazonaws.com/Ching_Editorial_style_front-view_medium-full_photo_shot_on_Fuji_20d54cb3-5f9d-41ff-a855-94d81cce6da8.png",
+    likes: 20,
+    views: 30,
+  },
+  {
+    id: 7,
+    title: "Post 7",
+    prompt:
+      "A Chinese 20-year-old Woman, looking like Audrey Hepburn, Black hair, standing on 2023 Tokyo street, hyper realistic portrait photography, pale skin, dress, wide shot, natural lighting, kodak portra 800, 105 mm f1. 8 , 32k--ar 16:9 --v 5 --s 750 --q 2",
+    imageUrl:
+      "https://ai-prompt-newsfeed.s3.us-west-2.amazonaws.com/Ching_A_Chinese_20-year-old_Woman_looking_like_Audrey_Hepburn_B_764b51ba-051e-444a-b697-efb049a3e55c.png",
+    likes: 20,
+    views: 30,
+  },
 ]
 
 export { posts }
