23da7e98bf8a22fc16a6e48010ebab79e882c1b9
youngchingjui <young.chingjui@gmail.com>
2023-04-01 12:30:37 +0800

    fixes imports

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,8 +1,8 @@
 import multer from "multer"
 import nextConnect from "next-connect"
 
-import { insertPost } from "../../lib/insertPost"
-import uploadImageToS3 from "../../lib/uploadImageToS3"
+import { insertPost } from "../../../lib/insertPost"
+import uploadImageToS3 from "../../../lib/uploadImageToS3"
 
 const upload = multer()
 
