70d45e4a06339f0a4c59236a7b93c49693813391
youngchingjui <young.chingjui@gmail.com>
2023-04-01 22:44:58 +0800

    disabled like button if not signed in

--- components/LikeButton.js
+++ components/LikeButton.js
@@ -1,11 +1,15 @@
+import { useSession } from "next-auth/react"
 import { useState } from "react"
 import Button from "react-bootstrap/Button"
+import OverlayTrigger from "react-bootstrap/OverlayTrigger"
+import Tooltip from "react-bootstrap/Tooltip"
 
 import updateLikeCount from "../lib/updateLikeCount"
 
 const LikeButton = ({ postId, likes }) => {
   const [isLiked, setIsLiked] = useState(false)
   const [likeCount, setLikeCount] = useState(likes)
+  const { data: session } = useSession()
 
   const handleLikeClick = async () => {
     const newLikeCount = isLiked ? likeCount - 1 : likeCount + 1
@@ -14,6 +18,23 @@ const LikeButton = ({ postId, likes }) => {
     updateLikeCount(postId, newLikeCount)
   }
 
+  const renderTooltip = (props) => (
+    <Tooltip id="like-tooltip" {...props}>
+      Sign in to like this post.
+    </Tooltip>
+  )
+
+  const disabledButton = (
+    <Button variant="outline-secondary">{likeCount} Likes</Button>
+  )
+
+  if (!session) {
+    return (
+      <OverlayTrigger placement="top" overlay={renderTooltip}>
+        {disabledButton}
+      </OverlayTrigger>
+    )
+  }
   return (
     <Button
       variant={isLiked ? "primary" : "outline-primary"}
