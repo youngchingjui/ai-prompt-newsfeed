5c800b72eefb11ee8905e0b7d18c5d6b6f7bda04
youngchingjui <young.chingjui@gmail.com>
2023-04-02 23:46:38 +0800

    create user on database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 
+import checkUserExists from "../../../lib/checkUserExists"
+import createUser from "../../../lib/createUser"
+
 export const authOptions = {
   providers: [
     DiscordProvider({
@@ -8,6 +11,29 @@ export const authOptions = {
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email, name, image } = user
+      const { provider } = account
+      const { username } = profile
+
+      // Check if user is already registered
+      checkUserExists(email).then((userExists) => {
+        // If not yet registered, create a new user in postgres
+        if (!userExists) {
+          createUser({
+            email,
+            username,
+            name,
+            image,
+            provider,
+          })
+        }
+      })
+
+      return true
+    },
+  },
 }
 
 export default NextAuth(authOptions)
