1b75e1062d02da36bf5bd6ab3e257d584a065457
youngchingjui <young.chingjui@gmail.com>
2023-04-04 14:50:29 +0800

    implement auth with database

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,39 +1,16 @@
 import NextAuth from "next-auth"
 import DiscordProvider from "next-auth/providers/discord"
 
-import checkUserExists from "../../../lib/checkUserExists"
-import createUser from "../../../lib/createUser"
+import { PostgresAdapter } from "../../../lib/pgAdapter/index"
 
 export const authOptions = {
+  adapter: PostgresAdapter(),
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
     }),
   ],
-  callbacks: {
-    async signIn({ user, account, profile }) {
-      const { email, name, image } = user
-      const { provider } = account
-      const { username } = profile
-
-      // Check if user is already registered
-      checkUserExists(email).then((userExists) => {
-        // If not yet registered, create a new user in postgres
-        if (!userExists) {
-          createUser({
-            email,
-            username,
-            name,
-            image,
-            provider,
-          })
-        }
-      })
-
-      return true
-    },
-  },
 }
 
 export default NextAuth(authOptions)
