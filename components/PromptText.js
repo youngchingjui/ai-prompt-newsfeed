6b274a441a76d1e2d7e8e4f8cf4eafad7afb9d8a
youngchingjui <young.chingjui@gmail.com>
2023-04-06 16:44:54 +0800

    added copy text functionality

--- components/PromptText.js
+++ components/PromptText.js
@@ -1,17 +1,38 @@
+import { useRef, useState } from "react"
+import Overlay from "react-bootstrap/Overlay"
+import Tooltip from "react-bootstrap/Tooltip"
+
 const PromptText = ({ children, props }) => {
+  const [copied, setCopied] = useState(false)
+  const textTarget = useRef(null)
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(children)
+    setCopied(true)
+    setTimeout(() => setCopied(false), 3000)
+  }
+
   return (
-    <p
-      style={{
-        color: "#888",
-        fontSize: "18px",
-        lineHeight: "1.2",
-        marginBottom: "1rem",
-        marginTop: "0.5rem",
-      }}
-      {...props}
-    >
-      {children}
-    </p>
+    <>
+      <p
+        style={{
+          color: "#888",
+          fontSize: "18px",
+          lineHeight: "1.2",
+          marginBottom: "1rem",
+          marginTop: "0.5rem",
+          cursor: "pointer",
+        }}
+        onClick={handleCopy}
+        ref={textTarget}
+        {...props}
+      >
+        {children}
+      </p>
+      <Overlay target={textTarget.current} show={copied} placement="top">
+        {(props) => <Tooltip {...props}>Copied!</Tooltip>}
+      </Overlay>
+    </>
   )
 }
 
